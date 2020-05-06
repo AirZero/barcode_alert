ff12a142476fdb4d6baf7c92b1756099ca6cb09c
airzero <airzero@email.com>
2020-05-06 10:53:37 +0300

    dual compare

---
+++ dual_compare.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+while :
+do
+        #clean_stdin
+        echo "Enter barcode"
+        read -r response </dev/tty
+                if grep -qi "$response" pienissd.txt; then
+                        echo "worked"
+#                       eval $command
+                        ./voice.sh "pieni levy";
+                fi
+        read -r response </dev/tty
+                if grep -qi "$response" lisensiton.txt; then
+                        echo "worked"
+#                       eval $command
+                        ./voice.sh "ei lisenssiä";
+                fi
+
+
+done
+
