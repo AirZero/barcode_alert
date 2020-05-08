66e7f3bc290b68e9c81d4effda371a966ebe213d
airzero <airzero@email.com>
2020-05-08 16:06:27 +0300

    working cellcomparer

--- cellcompare.sh
+++ cellcompare.sh
@@ -5,15 +5,12 @@ do
 	echo "Enter barcode"
 	read -r response </dev/tty
 		if grep -qi "$response" deviceinfo.txt; then
-			grep "$response" deviceinfo.txt|cat|sed 's/^.\{,9\}//'>info.txt;
-			./voice.sh cat info.txt;
+			grep "$response" deviceinfo.txt|cat|sed 's/^.\{,9\}//'>onedevice.csv;
+			./voice.sh cat onedevice.csv;
                 	echo "$worked"
 			echo "$response"
-		fi;
-		echo "$response";
-		IFS=' ' read -r -a array <<< "$response";
-#		for arg in "$@"
-#		do #echo ${array[\"${arg}"]}
-#		done
+		fi
+		arguments=$1","$2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12","$13","$14","$15","$16","$17","$18","$19","$20","$21","$22","$23","$24
+		csvcut -d "|" -c $arguments onedevice.csv|espeak-ng
 done
 
