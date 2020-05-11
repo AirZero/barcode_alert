cf0f3a3f6105c4b80ba546b848dd4f12d834235e
Eetu Rantakangas <eranta@stretch-6mRRdxT0>
2020-05-11 10:16:12 +0300

    alert comparer works

--- alert_compare.sh
+++ alert_compare.sh
@@ -5,9 +5,13 @@ do
 	#clean_stdin
 	echo "Enter barcode"
 	read -r response </dev/tty
+		"$response" >> scanned_devices.txt
 		if grep -qi "$response" ../prossu_muisti_ssd.csv; then
                 	echo "worked"
 			grep "$response" ../prossu_muisti_ssd.csv|cat|sed 's/^.\{,9\}//'>alertdata.csv;
 			#./voice.sh $puhe;
+			audio=$(cat alertdata.csv)
+			./voice.sh "$audio" ;
+
 		fi
 done
