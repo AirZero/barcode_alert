380e7f2a6658584c330a8caf47c424a00f127de9
Eetu Rantakangas <eranta@stretch-SEm1vo53>
2020-05-19 12:35:33 +0300

    general alert supports grepping without zeros

--- general_alert.sh
+++ general_alert.sh
@@ -5,10 +5,11 @@ do
 	#clean_stdin
 	echo "Enter barcode"
 	read -r response </dev/tty
+                withoutzeros=$(echo "$response"|sed 's/^.\{,2\}//');
 		echo "$response">>laitteet.txt
-		if grep -qi "$response" ../halytykset.csv; then
+		if grep -qi "$withoutzeros" ../halytykset.csv; then
                 	echo "worked"
-			grep "$response" ../halytykset.csv|cat|sed 's/^.\{,9\}//'>data.csv;
+			grep "$withoutzeros" ../halytykset.csv|cat|sed 's/^.\{,9\}//'>data.csv;
 			#./voice.sh $puhe;
 			audio=$(cat data.csv)
 			./voice.sh "$audio" ;
